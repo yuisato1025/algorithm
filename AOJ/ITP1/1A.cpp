50399567fbbd42b2f876eeac6b9f705c0f900b8d
Yui Sato <yuui-betterlatethannever@outlook.jp>
2023-10-21 20:35:05 +0900

    add AOJ/1A

---
+++ AOJ/ITP1/1A.cpp
@@ -0,0 +1,9 @@
+// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A&lang=en
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+    return 0;
+}
